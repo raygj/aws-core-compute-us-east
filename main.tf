5121acc13bd373da1b25d6208a6a37aba3b1eb92
Jim <ray.jim@gmail.com>
2020-03-01 21:53:08 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,9 +11,12 @@ resource "aws_instance" "test-ec2-instance" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
   key_name                    = var.key_name
-  subnet_id                   = aws_subnet.subnet-uno.id
-  vpc_security_group_ids      = [aws_security_group.ingress-all-test.id]
+  subnet_id                   = data.terraform_remote_state.subnet_id.outputs.subnet_id
+  vpc_security_group_ids      = [data.terraform_remote_state.vpc_security_group_ids.outputs.id]
   associate_public_ip_address = true
+
+  subnet_id =
+  vpc_id = data.terraform_remote_state.vpc.outputs.vpc_id
 
   tags = {
     Name  = "${var.owner}-demo_env"
@@ -26,7 +29,7 @@ resource "aws_instance" "test-ec2-instance" {
     type        = "ssh"
     user        = var.ssh_user
     private_key = var.private_key
-    host        = "${aws_instance.test-ec2-instance.public_ip}"
+    host        = "${data.terraform_remote_state.public_ip}"
     timeout     = "30s"
   }
   provisioner "remote-exec" {
@@ -36,72 +39,5 @@ resource "aws_instance" "test-ec2-instance" {
       "chmod +x ~/bootstrap.sh",
       "sudo ./bootstrap.sh"
     ]
-  }
-}
-
-output "elastic_IP_address" {
-  value = ["${aws_eip.ip-test-env.public_ip}"]
-}
-
-# network
-resource "aws_vpc" "test-env" {
-  cidr_block           = var.vpc_cidr
-  enable_dns_hostnames = true
-  enable_dns_support   = true
-}
-
-resource "aws_eip" "ip-test-env" {
-  instance = aws_instance.test-ec2-instance.id
-  vpc      = true
-}
-
-# subnets
-resource "aws_subnet" "subnet-uno" {
-  cidr_block        = cidrsubnet(aws_vpc.test-env.cidr_block, 3, 1)
-  vpc_id            = aws_vpc.test-env.id
-  availability_zone = "us-east-1a"
-}
-
-# gateways
-resource "aws_internet_gateway" "test-env-gw" {
-  vpc_id = aws_vpc.test-env.id
-}
-
-# route table
-resource "aws_route_table" "route-table-test-env" {
-  vpc_id = aws_vpc.test-env.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.test-env-gw.id
-  }
-}
-
-resource "aws_route_table_association" "subnet-association" {
-  subnet_id      = aws_subnet.subnet-uno.id
-  route_table_id = aws_route_table.route-table-test-env.id
-}
-
-# security
-resource "aws_security_group" "ingress-all-test" {
-  name   = "allow-all-sg"
-  vpc_id = aws_vpc.test-env.id
-
-  ingress {
-    cidr_blocks = [
-      "0.0.0.0/0", // any-any rule to specific IP of your host machine <not the world!>
-    ]
-
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-  }
-
-  // Terraform removes the default rule
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
   }
 }
